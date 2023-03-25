acc8d673a419f5d85bf0567630f8ae72236b5503
mariusCS50 <gaibumarius@gmail.com>
2023-03-25 20:49:13 +0200

    Revert "Update 2.5"
    This reverts commit db1370a295c418e4dd1a7240c6bf6ffa4c30b572.

--- app/src/main/java/com/example/nextstop/MainActivity.java
+++ app/src/main/java/com/example/nextstop/MainActivity.java
@@ -12,7 +12,6 @@ import android.content.pm.PackageManager;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
-import android.view.View;
 
 import org.osmdroid.config.Configuration;
 
@@ -31,8 +30,6 @@ public class MainActivity extends AppCompatActivity {
         if (actionBar != null) {
             actionBar.hide();
         }
-
-        setNavigationBarView();
 
         mapHelper = new MapHelper(this, findViewById(R.id.map));
         mapHelper.initializeDefaultMap();
@@ -60,16 +57,6 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    public void setNavigationBarView(){
-        this.getWindow().getDecorView().setSystemUiVisibility(
-                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                        | View.SYSTEM_UI_FLAG_FULLSCREEN
-                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
-    }
-
     @Override
     public void onPause() {
         super.onPause();
@@ -80,6 +67,5 @@ public class MainActivity extends AppCompatActivity {
     public void onResume() {
         super.onResume();
         mapHelper.onResume();
-        setNavigationBarView();
     }
 }
